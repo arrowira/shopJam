d542012c76ff018823c0447d2e8011b9c98d1650
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-28 11:12:50 -0700

    made shop scene

---
+++ new-game-project/scripts/shop.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		print("in")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
