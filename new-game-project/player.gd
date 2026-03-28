1118f7562ce8b4d9b0098b1098a7dea5cd952a23
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-27 23:33:14 -0700

    edible eating

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @export var speed = 20
 
+var size = 0
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -15,4 +17,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_eat_box_area_entered(area: Area2D) -> void:
-	print(area)
+	if area.is_in_group("edible"):
+		size += area.getValue()
+		print(size)
+		area.queue_free()
