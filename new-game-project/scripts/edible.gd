6a93599d7332ff447bedf257d54539fe25c437d7
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-29 09:06:08 -0700

    object spawning in medium shops

--- new-game-project/scripts/edible.gd
+++ new-game-project/scripts/edible.gd
@@ -3,8 +3,10 @@ extends Area2D
 var value
 
 func _ready() -> void:
+	if randf()<0.5:
+		queue_free()
 	$sprites.get_children()[randi_range(0,10)].visible = true
-	value = randi_range(5, 10)
+	value = randi_range(1, 5)
 
 func getValue():
 	return value
