f9804082d252379e3cdd6fc5e447e872661ca3eb
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-28 20:55:17 -0700

    edible shadows and stuff like that

--- new-game-project/scripts/edible.gd
+++ new-game-project/scripts/edible.gd
@@ -3,7 +3,7 @@ extends Area2D
 var value
 
 func _ready() -> void:
-	$sprites.get_children()
+	$sprites.get_children()[randi_range(0,10)].visible = true
 	value = randi_range(5, 10)
 
 func getValue():
