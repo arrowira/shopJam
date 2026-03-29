bf779986e415f6d0e2d372e3c21bf37bb895c71a
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-29 14:45:00 -0700

    fanfare

--- new-game-project/scripts/win_screen.gd
+++ new-game-project/scripts/win_screen.gd
@@ -3,11 +3,15 @@ extends Control
 var maxWeight = 400.0
 
 var fillAmt = 0
-
+var noFan = true
 	
 func _physics_process(delta: float) -> void:
 	if $ProgressBar.value < fillAmt:
 		$ProgressBar.value+=0.2
+	else:
+		if noFan and fillAmt != 0:
+			noFan = false
+			$AudioStreamPlayer.play()
 	$Panel/weight.text = str(int($ProgressBar.value*4))
 func win(weight):
 	Engine.time_scale = 0
