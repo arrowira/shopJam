b28fa9ca583a6934b2c3a1b22734070eec93493f
arrowira <41136800+arrowira@users.noreply.github.com>
2026-03-29 14:53:49 -0700

    start screen

--- new-game-project/scripts/start_screen.gd
+++ new-game-project/scripts/start_screen.gd
@@ -3,9 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimationPlayer.play("hide")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_start_time_timeout() -> void:
+	visible=false
